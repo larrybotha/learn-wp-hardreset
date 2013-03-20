fa54db7fbc589da2aab786503b536b36e4c8e802
Larry Botha <larrywp@themousepotatowebsite.co.za>
2013-03-20 07:26:26 +0200

    improve 404 text

--- 404.php
+++ 404.php
@@ -11,9 +11,9 @@ get_header(); ?>
 
 	<div class="content">
 
-		<h1><?php _e('404','theme_name'); ?></h1>
-		<p><?php _e( 'Page not found.', 'theme_name' ); ?></p>
-
+		<h1><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'theme_name' ); ?></h1>
+		<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'theme_name' ); ?></p>
+		<?php get_search_form(); ?>
 	</div><!-- .content -->
 
 <?php get_sidebar(); ?>
